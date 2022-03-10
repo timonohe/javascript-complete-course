cb24293e3e079494b8051ba65098796fcc1a8f6f
Timo Nohe <timo.ademeit@gmx.de>
2022-03-10 12:36:26 +0100

    27. Adding A Custom Function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,14 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function add(number1, number2) {
+    const result = number1 + number2;
+    alert(`The result is ${result}`);
+}
+
+add(1, 2);
+add(5, 5);
+
 currentResult = (currentResult + 10) * 3 / 2 - 1;
 
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
