8f3a635cf73efb96a749a041e3fc8d9817b607e9
Timo Nohe <timo.ademeit@gmx.de>
2022-03-24 12:47:53 +0100

    86. Adding More "if" Statements & A "Strong Attack" Functionality

--- monster-killer/assets/scripts/app.js
+++ monster-killer/assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 100;
@@ -7,8 +8,17 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler() {
-  const damage = dealMonsterDamage(ATTACK_VALUE);
+function attackMonster(mode) {
+  let maxDamage;
+  if (mode === 'ATTACK') {
+    maxDamage = ATTACK_VALUE;
+  } else if (mode === 'STRONG_ATTACK') {
+    maxDamage = STRONG_ATTACK_VALUE;
+  } else {
+    maxDamage = 0;
+  }
+
+  const damage = dealMonsterDamage(maxDamage);
   currentMonsterHealth -= damage;
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth -= playerDamage;
@@ -22,4 +32,13 @@ function attackHandler() {
   }
 }
 
+function attackHandler() {
+  attackMonster('ATTACK')
+}
+
+function strongAttackHandler() {
+  attackMonster('STRONG_ATTACK');
+}
+
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
