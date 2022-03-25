e34fc9b8a0586d1c465bde96e1bea4b8a11cca1d
Timo Nohe <timo.ademeit@gmx.de>
2022-03-25 08:27:23 +0100

    90. Validating User Input

--- monster-killer/assets/scripts/app.js
+++ monster-killer/assets/scripts/app.js
@@ -2,8 +2,14 @@ const ATTACK_VALUE = 10;
 const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
+const enteredValue = prompt('Maximum life for you and the monster.', '100');
 
-let chosenMaxLife = 100;
+let chosenMaxLife = parseInt(enteredValue);
+
+if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
+  chosenMaxLife = 100;
+}
+
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 let hasBonusLife = true;
