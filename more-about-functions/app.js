790fb3c0b1b276e80692e5c39608f700dc8b355a
Timo Nohe <timo.ademeit@gmx.de>
2022-04-02 13:03:07 +0200

    128. Function Expressions: Storing Functions in Variables

--- more-about-functions/app.js
+++ more-about-functions/app.js
@@ -1,12 +1,13 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-function startGame() {
+// [128] functions can be stored in variables like this
+const start = function() {
   console.log('Game is starting...');
-}
+};
 
+// [126] greet() is called "method" because it is stored within an object as a property
 // const person = { 
 //   name: 'Timo',
-//   // greet() is called "method" because it is stored within an object as a property
 //   greet: function greet() {
 //     console.log(`Hello ${this.name}!`);
 //   }
@@ -14,7 +15,11 @@ function startGame() {
 
 // person.greet()
 
-console.log(typeof startGame);
-console.dir(startGame);
+// [127] console-output shows that functions are objects with meta-informations in it
+// function startGame() {
+//   console.log('Game is starting...');
+// };
+// console.log(typeof startGame);
+// console.dir(startGame);
 
-startGameBtn.addEventListener('click', startGame);
+startGameBtn.addEventListener('click', start);
