3de00ba8f421b325440a516a1deef821f90973a6
Timo Nohe <timo.ademeit@gmx.de>
2022-04-07 12:51:07 +0200

    137. Introducing Rest Parameters ("Rest Operator")

--- more-about-functions/app.js
+++ more-about-functions/app.js
@@ -68,3 +68,27 @@ startGameBtn.addEventListener('click', () => {
   alert(message);
   gameIsRunning = false;
 });
+
+// not related to game
+
+// Introduction to the rest operator
+const sumUp = (a, b, ...numbers) => {
+  let sum = 0;
+  for (const num of numbers) {
+    sum += num;
+  }
+  return sum;
+};
+
+// same solution but with hidden arguments parameter
+const subtractUp = function() {
+  let sum = 0;
+  for (const num of arguments) {
+    sum -= num;
+  }
+  return sum;
+}
+
+console.log(sumUp(1, 5, 10, -3, 6, 10));
+console.log(sumUp(1, 5, 10, -3, 6, 10, 25, 88));
+console.log(subtractUp(1, 10, 15, 20));
