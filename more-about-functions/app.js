b65af1ba403de48735dd7e9493a8ed161fc05258
Timo Nohe <timo.ademeit@gmx.de>
2022-04-08 12:42:08 +0200

    139. Understanding Callback Functions

--- more-about-functions/app.js
+++ more-about-functions/app.js
@@ -72,7 +72,7 @@ startGameBtn.addEventListener('click', () => {
 // not related to game
 
 // Introduction to the rest operator
-const sumUp = (a, b, ...numbers) => {
+const sumUp = (resultHandler, ...numbers) => {
   const validateNumber = (number) => {
     return isNaN(number) ? 0 : number;
   };
@@ -81,7 +81,7 @@ const sumUp = (a, b, ...numbers) => {
   for (const num of numbers) {
     sum += validateNumber(num);
   }
-  return sum;
+  resultHandler(sum);
 };
 
 // same solution but with hidden arguments parameter
@@ -91,8 +91,12 @@ const subtractUp = function() {
     sum -= num;
   }
   return sum;
-}
+};
 
-console.log(sumUp(1, 5, 'fasd', 10, -3, 6, 10));
-console.log(sumUp(1, 5, 10, -3, 6, 10, 25, 88));
+const showResult = (result) => {
+  alert(`The result after adding all numbers is: ${result}`)
+};
+
+sumUp(showResult, 1, 5, 'fasd', 10, -3, 6, 10)
+sumUp(showResult, 1, 5, 10, -3, 6, 10, 25, 88)
 console.log(subtractUp(1, 10, 15, 20));
