5dfb175f508f1c8cf0f58351437bd04c7d55ba08
Timo Nohe <timo.ademeit@gmx.de>
2022-04-11 13:08:55 +0200

    assignment-05-dom-querying

---
+++ assignments/05-DOM-querying/assignment.js
@@ -0,0 +1,25 @@
+// Task 1
+const task1 = document.getElementById('task-1');
+task1.style.backgroundColor = 'black';
+task1.style.color = 'white';
+// const task1 = document.querySelector('#task-1');
+// task1.style.backgroundColor = 'black';
+// task1.style.color = 'white';
+// const task1 = document.querySelector('li');
+// task1.style.backgroundColor = 'black';
+// task1.style.color = 'white';
+
+// Task 2
+const docTitle = document.querySelector('title');
+docTitle.textContent = 'Assignment - Solved!';
+// document.title = 'Assignment - Solved!';
+// const title = document.querySelector('title');
+// title.textContent = 'Assignment - Solved!';
+// const title = document.head.querySelector('title');
+// title.text = 'Assignment - Solved!'
+
+// Task 3
+const h1 = document.getElementsByTagName('h1');
+h1[0].textContent = 'Assignment - Solved!';
+// const h1 = document.querySelector('h1');
+// h1.textContent = 'Assignment - Solved!';
