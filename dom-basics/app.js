3b8f75715106295c9f9001c7ad5f2ada264790ec
Timo Nohe <timo.ademeit@gmx.de>
2022-04-11 12:37:11 +0200

    153. Attributes vs Properties

--- dom-basics/app.js
+++ dom-basics/app.js
@@ -5,3 +5,14 @@ h1.className = 'title';
 console.log(h1.className);
 h1.style.color = 'white';
 h1.style.backgroundColor = 'red';
+
+const input = document.querySelector('input');
+console.dir(input);
+// That doesn't change the value-attribute of the input-tag
+input.value = 'Some other input the user might have entered';
+console.dir(input);
+h1.id = 'new-id';
+console.log(h1.id);
+input.setAttribute('value', 'some other default text');
+console.log(input.value);
+input.value = input.getAttribute('value');
