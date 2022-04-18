e01df016c3e405d71564fc3538f0821e8b18bfd9
Timo Nohe <timo.ademeit@gmx.de>
2022-04-18 12:35:54 +0200

    173. Controlling the Backdrop

--- dom-manipulation-practice/assets/scripts/app.js
+++ dom-manipulation-practice/assets/scripts/app.js
@@ -1,7 +1,22 @@
 const addMovieModal = document.getElementById('add-modal');
+const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
 const addMovieButton = document.querySelector('header button');
+const backdrop = document.getElementById('backdrop');
+
+const toggleBackdrop = () => {
+  backdrop.classList.toggle('visible');
+};
 const toggleMovieModal = () => {
   addMovieModal.classList.toggle('visible');
+  toggleBackdrop();
+};
+const backdropClickHandler = () => {
+  toggleMovieModal();
+};
+const cancelAddMovie = () => {
+  toggleMovieModal();
 };
 
 addMovieButton.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click', backdropClickHandler);
+cancelAddMovieButton.addEventListener('click', cancelAddMovie);
