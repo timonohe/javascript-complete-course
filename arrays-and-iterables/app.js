66087fa5478fa03b8715c13be6f383829551e191
Timo Nohe <timo.ademeit@gmx.de>
2022-04-20 12:39:11 +0200

    196. sort()ing and reverse()ing

--- arrays-and-iterables/app.js
+++ arrays-and-iterables/app.js
@@ -100,3 +100,15 @@ const taxAdjustedPricesWithMap = prices.map((price, index) => {
   return { index, taxAdjPrice: price * (1 + tax) };
 });
 console.log(prices, taxAdjustedPricesWithMap);
+
+const sortedPrices = prices.sort((a, b) => {
+  if (a > b) {
+    return 1;
+  } else if (a === b) {
+    return 0;
+  } else {
+    return -1;
+  }
+});
+console.log(sortedPrices);
+console.log(sortedPrices.reverse());
