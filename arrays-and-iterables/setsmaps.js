2964f61b2a97010a96e410a17bf8f99f125cf1f3
Timo Nohe <timo.ademeit@gmx.de>
2022-04-21 12:39:22 +0200

    206. Working with Maps

--- arrays-and-iterables/setsmaps.js
+++ arrays-and-iterables/setsmaps.js
@@ -1,13 +1,29 @@
+// A Set has unique entries
 const ids = new Set([1, 2, 3]);
 console.log(ids.has(1));
-// A Set has unique entries
 ids.add(2);
 console.log(ids.has(2));
 console.log(ids);
-
 for (const entry of ids.entries()) {
   console.log(entry);
 }
-
 ids.delete(3);
 console.log(ids);
+
+const person1 = { name: 'Max' };
+const person2 = { name: 'Manuel' };
+const personData = new Map([[person1, [{ date: 'yesterday', price: 10 }]]]);
+personData.set(person2, [{date: 'two weeks ago', price: 100}]);
+console.log(personData);
+console.log(personData.get(person1));
+console.log(personData.get(person2));
+for (const [key, value] of personData.entries()) {
+  console.log(key, value);
+}
+for (const key of personData.keys()) {
+  console.log(key);
+}
+for (const value of personData.values()) {
+  console.log(value);
+}
+personData.forEach((value) => console.log(value));
