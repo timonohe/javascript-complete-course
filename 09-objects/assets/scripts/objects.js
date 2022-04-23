eed53abaae4641ca31a2e0a56f4e960c03869fd6
Timo Nohe <timo.ademeit@gmx.de>
2022-04-23 13:03:07 +0200

    216. Adding, Modifying & Deleting Properties

--- 09-objects/assets/scripts/objects.js
+++ 09-objects/assets/scripts/objects.js
@@ -1,4 +1,4 @@
-const person = {
+let person = {
   name: 'Timo',
   age: 30,
   hobbies: ['Sports', 'Cooking'],
@@ -8,3 +8,13 @@ const person = {
   }
 };
 person.greet();
+
+// Add a new property isAdmin to the person object
+person.isAdmin = true;
+console.log(person.isAdmin);
+// Removes property from person object
+delete person.age;  // equal to person.age = undefined
+console.log(person);
+console.log(person.age);
+person.age = null; // will be part of the person object again
+console.log(person);
