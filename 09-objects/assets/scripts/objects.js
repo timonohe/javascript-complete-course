fde435595e4dc80919d6191a54f784edb4edb06c
Timo Nohe <timo.ademeit@gmx.de>
2022-04-25 12:28:10 +0200

    217. Special Key Names & Square Bracket Property Access

--- 09-objects/assets/scripts/objects.js
+++ 09-objects/assets/scripts/objects.js
@@ -18,3 +18,14 @@ console.log(person);
 console.log(person.age);
 person.age = null; // will be part of the person object again
 console.log(person);
+
+let specialKeyNamesObject = {
+  'first-name': 'Timo'
+};
+console.log(specialKeyNamesObject);
+console.log(specialKeyNamesObject['first-name']);
+const movieList = document.getElementById('movie-list');
+// movieList.style.backgroundColor = 'red';
+// movieList.style['backgroundColor'] = 'red';
+movieList.style['background-color'] = 'red';
+movieList.style.display = 'block';
