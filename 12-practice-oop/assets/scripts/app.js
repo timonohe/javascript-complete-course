2ff977e1ed5bf48885d34662be0ae61e613834d1
Timo Nohe <timo.ademeit@gmx.de>
2022-05-31 12:43:46 +0200

    281. Starting with the "Switch Project" Logic

--- 12-practice-oop/assets/scripts/app.js
+++ 12-practice-oop/assets/scripts/app.js
@@ -1,11 +1,42 @@
 class Tooltip {}
 
-class ProjectItem {}
+class ProjectItem {
+  constructor(id) {
+    this.id = id;
+    this.connectSwitchButton();
+    this.connectMoreInfoButton();
+  }
+
+  connectMoreInfoButton() {
+
+  }
+
+  connectSwitchButton() {
+    const projectItemElement = document.getElementById(this.id);
+    const switchBtn = projectItemElement.querySelector('button:last-of-type');
+    switchBtn.addEventListener('click', )
+  }
+}
 
 class ProjectList {
+  projects = [];
+
   constructor(type) {
     const prjItems = document.querySelectorAll(`#${type}-projects li`);
-    console.log(prjItems);
+    for (const prjItem of prjItems) {
+      this.projects.push(new ProjectItem(prjItem.id));
+    }
+    console.log(this.projects);
+  }
+
+  addProject() {
+
+  }
+
+  switchProject(projectId) {
+    // const projectIndex = this.projects.findIndex(p => p.id === projectId);
+    // this.projects.splice(projectIndex, 1);
+    this.projects = this.projects.filter(p => p.id !== projectId);
   }
 }
 
