3ef6b6983d0887e253d9a1ec37ffbb47634af080
Timo Nohe <timo.ademeit@gmx.de>
2022-05-31 12:17:06 +0200

    275. Setting & Getting Prototypes

--- 11-prototypes/app.js
+++ 11-prototypes/app.js
@@ -55,10 +55,46 @@ class Person extends AgedPerson {
 // console.dir(Object);
 // console.dir(Object.prototype);
 
-const p = new Person();
-const p2 = new Person();
-console.log(p);
-console.log(p.__proto__ === p2.__proto__);
+// const p = new Person();
+// const p2 = new Person();
+// console.log(p);
+// console.log(p.__proto__ === p2.__proto__);
 
-const button = document.getElementById('btn');
-button.addEventListener('click', p.greet);
+// const button = document.getElementById('btn');
+// button.addEventListener('click', p.greet);
+
+const course = {
+  title: 'JavaScript - The Complete Guide',
+  rating: 5
+};
+
+console.log(Object.getPrototypeOf(course)); // same as course.__prototype__
+Object.setPrototypeOf(course, {
+  ...Object.getPrototypeOf(course),
+  printRating: function() {
+    console.log(`${this.rating} out of 5`);
+  }
+});
+
+const student = Object.create({
+  printProgress: function() {
+    console.log(`${this.progress * 100}%`);
+  }
+}, {
+  name: {
+    configurable: true,
+    enumerable: true,
+    value: 'Timo',
+    writable: false
+  }
+});
+
+Object.defineProperty(student, 'progress', {
+  configurable: true,
+  enumerable: true,
+  value: 0.8,
+  writable: false
+});
+console.log(student);
+student.printProgress();
+course.printRating();
