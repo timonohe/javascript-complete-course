bfa84d1e3a72fb59d2743db2a4d168bd8f05f81c
Timo Nohe <timo.ademeit@gmx.de>
2022-06-05 12:47:06 +0200

    306. Different Ways of Listening to Events

---
+++ 14-events/assets/scripts/events.js
@@ -0,0 +1,14 @@
+const button = document.querySelector('button');
+// button.onclick = function() {
+//   alert('Hello there!');
+// }
+// const buttonClickHandler = () => {
+//   alert('Button was clicked!');
+// };
+// const anotherButtonClickHandler = () => {
+//   console.log('This was clicked!');
+// };
+// button.onclick = buttonClickHandler;
+// button.onclick = anotherButtonClickHandler;
+button.addEventListener();
+button.removeEventListener();
