dc453402ec79e0bef0b6a642cc332a45cdfe2e91
Timo Nohe <timo.ademeit@gmx.de>
2022-06-05 12:56:27 +0200

    307. Removing Event Listeners

--- 14-events/assets/scripts/events.js
+++ 14-events/assets/scripts/events.js
@@ -2,13 +2,16 @@ const button = document.querySelector('button');
 // button.onclick = function() {
 //   alert('Hello there!');
 // }
-// const buttonClickHandler = () => {
-//   alert('Button was clicked!');
-// };
-// const anotherButtonClickHandler = () => {
-//   console.log('This was clicked!');
-// };
+const buttonClickHandler = () => {
+  alert('Button was clicked!');
+};
+const anotherButtonClickHandler = () => {
+  console.log('This was clicked!');
+};
 // button.onclick = buttonClickHandler;
 // button.onclick = anotherButtonClickHandler;
-button.addEventListener();
-button.removeEventListener();
+button.addEventListener('click', buttonClickHandler);
+
+setTimeout(() => {
+  button.removeEventListener('click', buttonClickHandler);
+}, 2000);
