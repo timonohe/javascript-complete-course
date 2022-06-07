9cc84d2e3106e9a081ea666f1c1e5a478d1d370d
Timo Nohe <timo.ademeit@gmx.de>
2022-06-07 12:48:38 +0200

    313. Event Propagation & "stopPropagation()"

--- 14-events/assets/scripts/events.js
+++ 14-events/assets/scripts/events.js
@@ -1,4 +1,4 @@
-// const button = document.querySelector('button');
+const button = document.querySelector('button');
 // const buttons = document.querySelectorAll('button');
 
 // button.onclick = function() {
@@ -27,5 +27,16 @@
 const form = document.querySelector('form');
 form.addEventListener('submit', event => {
   event.preventDefault();
+  console.log(event);
+});
+
+const div = document.querySelector('div');
+div.addEventListener('click', event => {
+  console.log('CLICKED DIV');
+  console.log(event);
+});
+button.addEventListener('click', event => {
+  event.stopPropagation();
+  console.log('CLICKED BUTTON');
   console.log(event);
 });
