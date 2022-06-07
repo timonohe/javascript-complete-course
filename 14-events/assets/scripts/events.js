ce79cbb890d91de13fb8af2def115bf3617e6ca1
Timo Nohe <timo.ademeit@gmx.de>
2022-06-07 12:29:21 +0200

    309. Supported Event Types

--- 14-events/assets/scripts/events.js
+++ 14-events/assets/scripts/events.js
@@ -5,7 +5,7 @@ const buttons = document.querySelectorAll('button');
 //   alert('Hello there!');
 // }
 const buttonClickHandler = event => {
-  event.target.disabled = true;
+  // event.target.disabled = true;
   console.log(event);
 };
 const anotherButtonClickHandler = () => {
@@ -19,4 +19,8 @@ const anotherButtonClickHandler = () => {
 //   button.removeEventListener('click', buttonClickHandler);
 // }, 2000);
 
-buttons.forEach(btn => btn.addEventListener('click', buttonClickHandler));
+buttons.forEach(btn => btn.addEventListener('mouseenter', buttonClickHandler));
+
+window.addEventListener('scroll', event => {
+  console.log(event);
+});
