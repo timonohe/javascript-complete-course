9ae3ece0f2da13f71b2e0581b6de8c41d7abd191
Timo Nohe <timo.ademeit@gmx.de>
2022-06-10 12:48:14 +0200

    337. How Numbers Work & Behave in JavaScript

---
+++ 16-numbers-srings/app.js
@@ -0,0 +1,5 @@
+console.log('Biggest Integer (without decimal places)', Number.MAX_SAFE_INTEGER);
+console.log('Largest Number (with decimal places)', Number.MAX_VALUE);
+
+console.log('Smallest Integer (without decimal places)', Number.MIN_SAFE_INTEGER);
+console.log('Smallest Number (with decimal places)', Number.MIN_VALUE);
