98587d87487332c9eea553610cfc93817cdeee39
Timo Nohe <timo.ademeit@gmx.de>
2022-06-21 12:34:55 +0200

    354. Getting Started with Promises

--- 17-async/app.js
+++ 17-async/app.js
@@ -1,18 +1,23 @@
 const button = document.querySelector('button');
 const output = document.querySelector('p');
 
+const setTimer = duration => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Done!');
+    }, duration);
+  });
+  return promise;
+};
+
 function trackUserHandler() {
   navigator.geolocation.getCurrentPosition(
     posData => {
-      setTimeout(() => {
-        console.log(posData);
-      }, 2000);
+      setTimer(2000).then(data => console.log(data, posData));
     },
     error => console.error(error)
   );
-  setTimeout(() => {
-    console.log('Timer done!');
-  }, 0);
+  setTimer(1000).then(() => console.log('Timer done!'));
   console.log('Getting position...');
 }
 
