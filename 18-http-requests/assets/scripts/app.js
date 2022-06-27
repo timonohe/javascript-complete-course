08d6f3ad0f0bb3498ca45cd67c8b74dd48f8538d
Timo Nohe <timo.ademeit@gmx.de>
2022-06-27 12:25:34 +0200

    373. Sending Data with a POST Request

--- 18-http-requests/assets/scripts/app.js
+++ 18-http-requests/assets/scripts/app.js
@@ -1,7 +1,7 @@
 const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
 
-function sendHttpRequest(method, url) {
+function sendHttpRequest(method, url, data) {
   const promise = new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
     xhr.open(method, url);
@@ -11,31 +11,41 @@ function sendHttpRequest(method, url) {
       resolve(xhr.response);
     };
     
-    xhr.send();
+    xhr.send(JSON.stringify(data));
   });
   return promise;
 }
 
-function fetchPosts() {
-  sendHttpRequest('GET', 'https://jsonplaceholder.typicode.com/posts')
-    .then(resposneData => {
-            // const listOfPosts = xhr.response;
-      // xhr.response returns a JSON-Object not a native array --> whould throws an error
-      // listOfPosts.push({});
-      
-      // this line is not necessary if responseType is 'json'
-      // const listOfPosts = JSON.parse(xhr.response);
-    
-      const listOfPosts = resposneData;
-      // console.log(listOfPosts);
-    
-      for (const post of listOfPosts) {
-        const postEl = document.importNode(postTemplate.content, true);
-        postEl.querySelector('h2').textContent = post.title.toUpperCase();
-        postEl.querySelector('p').textContent = post.body;
-        listElement.append(postEl);
-      }
-    });
+async function fetchPosts() {
+  const responseData = await sendHttpRequest('GET', 'https://jsonplaceholder.typicode.com/posts');
+
+  // const listOfPosts = xhr.response;
+  // xhr.response returns a JSON-Object not a native array --> whould throws an error
+  // listOfPosts.push({});
+  
+  // this line is not necessary if responseType is 'json'
+  // const listOfPosts = JSON.parse(xhr.response);
+
+  const listOfPosts = responseData;
+  // console.log(listOfPosts);
+
+  for (const post of listOfPosts) {
+    const postEl = document.importNode(postTemplate.content, true);
+    postEl.querySelector('h2').textContent = post.title.toUpperCase();
+    postEl.querySelector('p').textContent = post.body;
+    listElement.append(postEl);
+  }
+}
+
+async function createPost(title, content) {
+  const userId = Math.random();
+  const post = {
+    title: title,
+    body: content,
+    userId: userId
+  }
+  sendHttpRequest('POST', 'https://jsonplaceholder.typicode.com/posts', post);
 }
 
 fetchPosts();
+createPost('DUMMY', 'A dummy post!');
