2a7d858e8d476cbeaf47f272c468cc69e58fa85d
Timo Nohe <timo.ademeit@gmx.de>
2022-06-27 12:18:55 +0200

    372. Promisifying Http Requests (with XMLHttpRequest)

--- 18-http-requests/assets/scripts/app.js
+++ 18-http-requests/assets/scripts/app.js
@@ -1,27 +1,41 @@
 const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
 
-const xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
-xhr.responseType = 'json';
+function sendHttpRequest(method, url) {
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.responseType = 'json';
+    
+    xhr.onload = function() {
+      resolve(xhr.response);
+    };
+    
+    xhr.send();
+  });
+  return promise;
+}
 
-xhr.onload = function() {
-  // const listOfPosts = xhr.response;
-  // xhr.response returns a JSON-Object not a native array --> whould throws an error
-  // listOfPosts.push({});
-  
-  // this line is not necessary if responseType is 'json'
-  // const listOfPosts = JSON.parse(xhr.response);
+function fetchPosts() {
+  sendHttpRequest('GET', 'https://jsonplaceholder.typicode.com/posts')
+    .then(resposneData => {
+            // const listOfPosts = xhr.response;
+      // xhr.response returns a JSON-Object not a native array --> whould throws an error
+      // listOfPosts.push({});
+      
+      // this line is not necessary if responseType is 'json'
+      // const listOfPosts = JSON.parse(xhr.response);
+    
+      const listOfPosts = resposneData;
+      // console.log(listOfPosts);
+    
+      for (const post of listOfPosts) {
+        const postEl = document.importNode(postTemplate.content, true);
+        postEl.querySelector('h2').textContent = post.title.toUpperCase();
+        postEl.querySelector('p').textContent = post.body;
+        listElement.append(postEl);
+      }
+    });
+}
 
-  const listOfPosts = xhr.response;
-  // console.log(listOfPosts);
-
-  for (const post of listOfPosts) {
-    const postEl = document.importNode(postTemplate.content, true);
-    postEl.querySelector('h2').textContent = post.title.toUpperCase();
-    postEl.querySelector('p').textContent = post.body;
-    listElement.append(postEl);
-  }
-};
-
-xhr.send();
+fetchPosts();
