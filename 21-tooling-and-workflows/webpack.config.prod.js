b51f8f7ccb9144b7f17ce7869ba6310936de62b1
Timo Nohe <timo.ademeit@gmx.de>
2022-07-01 13:13:33 +0200

    422. Building For Production

---
+++ 21-tooling-and-workflows/webpack.config.prod.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/app.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'assets', 'scripts'),
+    publicPath: 'assets/scripts/'
+  },
+  devtool: 'cheap-source-map'
+};
