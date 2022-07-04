d9bc31e74a00ac2e042b4fb293a118598e24baba
Timo Nohe <timo.ademeit@gmx.de>
2022-07-04 12:51:02 +0200

    431. Working with Cookies

--- 22-browser-storage/app.js
+++ 22-browser-storage/app.js
@@ -3,9 +3,20 @@ const retrCookieBtn = document.getElementById('retrieve-btn');
 
 storeCookieBtn.addEventListener('click', () => {
   const userId = 'u123';
-  document.cookie = `uid=${userId}`;
+  const user = {
+    name: 'Timo',
+    age: 31
+  };
+  document.cookie = `uid=${userId}; max-age=360`;
+  document.cookie = `user=${JSON.stringify(user)}`;
 });
 
 retrCookieBtn.addEventListener('click', () => {
   console.log(document.cookie);
+  const cookieData = document.cookie.split(';');
+  const data = cookieData.map(i => {
+    return i.trim();
+  });
+  console.log(data);
+  console.log(data[1].split('=')[1]);
 });
