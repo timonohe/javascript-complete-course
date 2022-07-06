2da976a6a8165e153d6dcde5f61c64762f34e440
Timo Nohe <timo.ademeit@gmx.de>
2022-07-06 12:44:44 +0200

    452. Adding Feedback (Showing a Modal)

--- 24-time-to-practice/src/SharePlace.js
+++ 24-time-to-practice/src/SharePlace.js
@@ -1,3 +1,5 @@
+import { Modal } from './UI/Modal';
+
 class PlaceFinder {
   constructor() {
     const adressForm = document.querySelector('form');
@@ -12,8 +14,11 @@ class PlaceFinder {
       alert('Location feature is not available in your browser - please use a more modern browser or manually enter an address.');
       return;
     }
+    const modal = new Modal('loading-modal-content', 'Loading location - please wait!');
+    modal.show();
     navigator.geolocation.getCurrentPosition(
       successResult => {
+        modal.hide();
         const coordinates = {
           lat: successResult.coords.latitude,
           lng: successResult.coords.longitude
@@ -21,6 +26,7 @@ class PlaceFinder {
         console.log(coordinates);
       },
       error => {
+        modal.hide();
         alert('Could not locate you unfortunately. Please enter an address manually!');
       }
     );
