0121c55832ebf1ecaab1b57e86abef5d05ae005f
Timo Nohe <timo.ademeit@gmx.de>
2022-07-06 12:30:29 +0200

    451. Getting the User Location

--- 24-time-to-practice/src/SharePlace.js
+++ 24-time-to-practice/src/SharePlace.js
@@ -8,10 +8,27 @@ class PlaceFinder {
   }
 
   locateUserHandler() {
-
+    if (!navigator.geolocation) {
+      alert('Location feature is not available in your browser - please use a more modern browser or manually enter an address.');
+      return;
+    }
+    navigator.geolocation.getCurrentPosition(
+      successResult => {
+        const coordinates = {
+          lat: successResult.coords.latitude,
+          lng: successResult.coords.longitude
+        };
+        console.log(coordinates);
+      },
+      error => {
+        alert('Could not locate you unfortunately. Please enter an address manually!');
+      }
+    );
   }
 
   findAdressHandler() {
 
   }
 }
+
+new PlaceFinder();
