cdf98d02f2f594f6a6924496ad50afa5d3d5edf8
Timo Nohe <timo.ademeit@gmx.de>
2022-07-08 13:00:05 +0200

    470. Understanding Symbols

---
+++ 26-meta-programming/app.js
@@ -0,0 +1,15 @@
+// Library land
+const uid = Symbol('uid');
+console.log(uid);
+
+const user = {
+  // id: 'p1',
+  [uid]: 'p1',
+  name: 'Timo',
+  age: 31
+};
+
+// app land => Using the library
+user.id = 'p2'; // this should not be possible!
+console.log(user);
+console.log(Symbol('uid') === Symbol('uid'));
