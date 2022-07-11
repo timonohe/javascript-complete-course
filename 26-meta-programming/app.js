7959b8c082c08351aa0f96a32a3ff9719fb5765a
Timo Nohe <timo.ademeit@gmx.de>
2022-07-11 13:13:19 +0200

    477. Working with Proxy Traps

--- 26-meta-programming/app.js
+++ 26-meta-programming/app.js
@@ -90,7 +90,15 @@ const courseHandler = {
       return 0;
     }
     return obj[propertyName] ||'NOT FOUND';
+  },
+  set(obj, propertyName, newValue) {
+    console.log('Sending data ...');
+    if (propertyName === 'rating') {
+      return;
+    }
+    obj[propertyName] = newValue;
   }
 };
 const pCourse = new Proxy(course, courseHandler);
+pCourse.rating = 5;
 console.log(pCourse.title, pCourse.length, pCourse.rating);
