1e7e511d52c915e96a16b5f78de21897f494ce1c
Timo Nohe <timo.ademeit@gmx.de>
2022-07-11 12:27:41 +0200

    471. Well-known Symbols

--- 26-meta-programming/app.js
+++ 26-meta-programming/app.js
@@ -6,10 +6,12 @@ const user = {
   // id: 'p1',
   [uid]: 'p1',
   name: 'Timo',
-  age: 31
+  age: 31,
+  [Symbol.toStringTag]: 'User'
 };
 
 // app land => Using the library
 user.id = 'p2'; // this should not be possible!
 console.log(user);
 console.log(Symbol('uid') === Symbol('uid'));
+console.log(user.toString());
