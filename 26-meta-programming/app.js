4b167975a64452d45166526c4bd4205eae0b43dd
Timo Nohe <timo.ademeit@gmx.de>
2022-07-11 12:46:32 +0200

    473. Generators & Iterable Objects

--- 26-meta-programming/app.js
+++ 26-meta-programming/app.js
@@ -17,25 +17,51 @@ console.log(Symbol('uid') === Symbol('uid'));
 console.log(user.toString());
 
 const company = {
-  curEmployee: 0,
+  // curEmployee: 0,
   employees: ['Max', 'Manu', 'Anna'],
-  next() {
-    if (this.curEmployee >= this.employees.length) {
-      return { value: this.curEmployee, done: true };
-    }
+  // next() {
+  //   if (this.curEmployee >= this.employees.length) {
+  //     return { value: this.curEmployee, done: true };
+  //   }
 
-    const returnValue = { 
-      value: this.employees[this.curEmployee], 
-      done: false
-    };
-    this.curEmployee++;
-    return returnValue;
+  //   const returnValue = { 
+  //     value: this.employees[this.curEmployee], 
+  //     done: false
+  //   };
+  //   this.curEmployee++;
+  //   return returnValue;
+  // },
+  [Symbol.iterator]: function* employeeGenerator() {
+    // let employee = company.next();
+
+    // while(!employee.done) {
+    //   yield employee.value;
+    //   employee = company.next();
+    // }
+    let currentEmployee = 0;
+    while(currentEmployee < this.employees.length) {
+      yield this.employees[currentEmployee];
+      currentEmployee++;
+    }
   }
 }
 
-let employee = company.next();
+// let employee = company.next();
 
-while(!employee.done) {
-  console.log(employee.value);
-  employee = company.next();
+// while(!employee.done) {
+//   console.log(employee.value);
+//   employee = company.next();
+// }
+
+for (const employee of company) {
+  console.log(employee);
 }
+console.log([...company]);
+
+// const it = company.getEmployee();
+
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
