76374ed1bff0d463fbc6ac84487eaaadcacecc1f
Timo Nohe <timo.ademeit@gmx.de>
2022-07-12 12:21:28 +0200

    483. Understanding Modules & File Access

--- 27-nodejs/app.js
+++ 27-nodejs/app.js
@@ -1,2 +1,21 @@
+const fs = require('fs');
+
 const userName = 'Timo';
 console.log(`Hi ${userName}!`);
+
+fs.writeFile('user-data.txt', 'username=Timo', err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Wrote to file!');
+  }
+});
+
+fs.readFile('user-data.txt', (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log(data.toString());
+});
