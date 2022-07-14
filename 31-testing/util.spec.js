28e9333c47e420c9fb619d47fc520a1a1046b236
Timo Nohe <timo.ademeit@gmx.de>
2022-07-14 12:56:50 +0200

    535. Writing & Running Integration Tests

--- 31-testing/util.spec.js
+++ 31-testing/util.spec.js
@@ -1,6 +1,11 @@
-const { generateText } = require('./util');
+const { generateText, checkAndGenerate } = require('./util');
 
 test('should output name and age', () => {
   const text = generateText('Timo', 31);
+  expect(text).toBe('Timo (31 years old)');
+});
+
+test('should generate a valid text output', () => {
+  const text = checkAndGenerate('Timo', 31);
   expect(text).toBe('Timo (31 years old)');
 });
