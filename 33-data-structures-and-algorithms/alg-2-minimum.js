27b69fdf7bd5dce346b5136a9b8f956fc8ebd7d7
Timo Nohe <timo.ademeit@gmx.de>
2022-07-21 12:23:51 +0200

    550. Solving the Same Problem Differently

--- 33-data-structures-and-algorithms/alg-2-minimum.js
+++ 33-data-structures-and-algorithms/alg-2-minimum.js
@@ -14,6 +14,29 @@ function getMin(numbers) {
   return currentMinimum;
 }
 
+function getMin2(numbers) {
+  if (!numbers.length) {
+    throw new Error('Should not be an empty array!');
+  }
+
+  for (let i = 0; i < numbers.length; i++) {
+    let outerElement = numbers[i];
+
+    for (let j = i + 1; j < numbers.length; j++) {
+      let innerElement = numbers[j];
+
+      if (outerElement > innerElement) {
+        numbers[i] = innerElement;
+        numbers[j]= outerElement;
+        outerElement = numbers[i];
+        innerElement = numbers[j];
+      }
+    }
+  }
+
+  return numbers[0];
+}
+
 const testNumbers = [3, 1, 2];
 const min = getMin(testNumbers);
 console.log(min); // should be 1
@@ -26,3 +49,9 @@ try {
 }
 console.log(getMin([1, 5, 1]));
 console.log(getMin([10, -5, 100, 99, -2]));
+
+console.log(getMin2([1, 3, 2]));
+console.log(getMin2([2, 1, -5, 10, 10, -10]));
+console.log(getMin2([1]));
+console.log(getMin2([1, 1, 1, 5]));
+console.log(getMin2([5, 1, 5]));
