e5cd69ee32448c7069633d7d38662cd6a0ed3946
Timo Nohe <timo.ademeit@gmx.de>
2022-07-21 16:28:33 +0200

    561. Core Types & Diving Deeper

--- 34-typescript-introduction/app.ts
+++ 34-typescript-introduction/app.ts
@@ -1,7 +1,24 @@
+const num1Input = document.getElementById('num1') as HTMLInputElement;
+const num2Input = <HTMLInputElement>document.getElementById('num2');
+const buttonElement = document.querySelector('button') as HTMLButtonElement;
+
 function add(a: number, b: number) {
   return a + b;
 }
 
-const result = add(5, 3);
+function printResult(result): void {
+  console.log(result);
+}
 
-console.log(result);
+// const result = add(5, 3);
+// let isDone = false;
+
+// printResult(result);
+
+buttonElement.addEventListener('click', () => {
+  const num1 = +num1Input.value;
+  const num2 = +num2Input.value;
+  const result = add(num1, num2);
+
+  printResult(result);
+});
