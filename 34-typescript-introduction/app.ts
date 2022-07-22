fe51d07f796ccbb47ea0b9dc76d971f1567f3f06
Timo Nohe <timo.ademeit@gmx.de>
2022-07-22 12:23:37 +0200

    564. Classes & Interfaces

--- 34-typescript-introduction/app.ts
+++ 34-typescript-introduction/app.ts
@@ -1,3 +1,38 @@
+// class User {
+//   name: string;
+//   private age: number;
+
+//   constructor(name: string, age: number) {
+//     this.name = name;
+//     this.age = age;
+//   }
+// }
+
+interface Greetable {
+  name: string;
+}
+
+interface Printable {
+  print(): void;
+}
+
+class User implements Greetable, Printable {
+  constructor(public name: string, private age: number) {}
+
+  print() {
+    console.log(this.name);
+  }
+}
+
+class Admin extends User {
+  constructor(name: string, age: number, private permissions: string[]) {
+    super(name, age);
+  }
+}
+
+const user = new User('Timo', 31);
+console.log(user.name);
+
 const num1Input = document.getElementById('num1') as HTMLInputElement;
 const num2Input = <HTMLInputElement>document.getElementById('num2');
 const buttonElement = document.querySelector('button') as HTMLButtonElement;
@@ -22,7 +57,12 @@ function printResult(result: string | number, printMode: OutputMode): void {
 
 // printResult(result);
 
-type CalculationResults = { res: number, print: () => void }[];
+interface CalculationContainer {
+  res: number;
+  print(): void;
+}
+
+type CalculationResults = CalculationContainer[];
 
 const results: CalculationResults = [];
 const names = ['Timo'];
